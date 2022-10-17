93a2cf0184534806b7c5359c79f409c1802f94bb
omarkhan5299 <mok236@nyu.edu>
2022-10-17 17:44:33 +0000

    Creates a Recommendation

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,9 @@ from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
 from service.models import db
-from service.common import status  # HTTP Status Codes
+from service.common import status
+from tests.factories import RecommendationFactory  # HTTP Status Codes
+from service import models
 
 
 ######################################################################
@@ -47,3 +49,22 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_recommendation(self):
+        """It should Create a new Recommendation"""
+        self.client = app.test_client()
+        test_recommendation = RecommendationFactory()
+        logging.debug("Test Recommendation: %s", test_recommendation.serialize())
+        response = self.client.post("/recommendations", json=test_recommendation.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check the data is correct
+        new_recommendation = response.get_json()
+        #self.assertEqual(new_recommendation["id"], test_recommendation.id)
+        self.assertEqual(new_recommendation["product_1"], test_recommendation.product_1)
+        self.assertEqual(new_recommendation["product_2"], test_recommendation.product_2)
+        #self.assertEqual(new_recommendation["recommendation_type"], test_recommendation.recommendation_type)
+
+
+
+
