171204da1d1e30f81eb6a8fecb63bada15b71ded
Rishika Pabba <60458215+rishikapabba@users.noreply.github.com>
2022-10-19 14:58:59 -0400

    added delete tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,4 +142,13 @@ class TestYourResourceServer(TestCase):
             self.assertEqual(updated_recommendation["liked"], False)
 
 
+    def test_delete_recommendation(self):
+        """It should Delete a Recommendation"""
+        test_recommendation = self._create_recommendations(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{test_recommendation.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_pet.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
